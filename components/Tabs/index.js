99bfbfe921cbb40b4e47c385dc1a4a9038523428
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-09 10:22:22 -0500

    Created axios call, captured and stored needed data and build component in tabs.js file

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then((res) => {
+	const topics = res.data.topics;
+	console.log(topics);
+	
+})
+
+function createTab(str){
+
+	// Create Element
+	const tab = document.createElement('div');
+
+	// Add class to element
+	tab.classList.add('tab');
+
+	// Add in content
+	tab.textContent = str;
+
+	return tab;
+}
