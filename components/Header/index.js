2c6534525b1c64949b6b3739692dd391b3802b31
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-09 10:13:40 -0500

    Build component logic for the header in index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+	
+	// Creating elements
+	const header = document.createElement('div');
+	const date = document.createElement('span');
+	const h1 = document.createElement('h1');
+	const temp = document.createElement('span');
+	
+	// Adding in classes to elements
+	header.classList.add('header');
+	date.classList.add('date');
+	temp.classList.add('temp');
+
+	// Elements Content
+	date.textContent = 'SMARCH 28, 2019';
+	h1.textContent = 'Lambda Times';
+	temp.textContent = '98°';
+
+	// Createing HTML Structure
+	header.appendChild(date);
+	header.appendChild(h1);
+	header.appendChild(temp);
+}
